904d443b22e0c1e523c7c9313d82a116a98b60c6
Mariel <Mariel.LatellaBarros@student.pxl.be>
2020-04-17 10:50:56 +0200

    Create and persist a message into the database using transactions

--- src/main/java/be/pxl/ja2/jpa/Opdracht1SaveMessage.java
+++ src/main/java/be/pxl/ja2/jpa/Opdracht1SaveMessage.java
@@ -1,9 +1,37 @@
 package be.pxl.ja2.jpa;
 
+import be.pxl.ja2.jpa.model.Message;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
 public class Opdracht1SaveMessage {
 
 	public static void main(String[] args) {
+		EntityManagerFactory entityManagerFactory = null;
+		EntityManager entityManager = null;
 
+		try {
+			entityManagerFactory = Persistence.createEntityManagerFactory("musicdb_pu");
+			entityManager = entityManagerFactory.createEntityManager();
+			EntityTransaction transaction = entityManager.getTransaction();
+
+			transaction.begin();
+
+			Message message = new Message(100, "Hello World");
+			entityManager.persist(message);
+			transaction.commit();
+			System.out.println("message saved");
+		}
+		finally {
+			if (entityManager != null) {
+				entityManager.close();
+			}
+			if (entityManagerFactory != null) {
+				entityManagerFactory.close();
+			}
+		}
 	}
-
 }

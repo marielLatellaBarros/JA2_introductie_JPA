d38e94e245067705b7117471a195adc67426b6b0
Mariel <Mariel.LatellaBarros@student.pxl.be>
2020-04-17 10:51:24 +0200

    Get a message from the database using transactions

--- src/main/java/be/pxl/ja2/jpa/Opdracht2GetMessage.java
+++ src/main/java/be/pxl/ja2/jpa/Opdracht2GetMessage.java
@@ -1,5 +1,35 @@
 package be.pxl.ja2.jpa;
 
-public class Opdracht2GetMessage {
+import be.pxl.ja2.jpa.model.Message;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+public class Opdracht2GetMessage {
+    public static void main(String[] args) {
+        EntityManagerFactory entityManagerFactory = null;
+        EntityManager entityManager = null;
+
+        try {
+            entityManagerFactory = Persistence.createEntityManagerFactory("musicdb_pu");
+            entityManager = entityManagerFactory.createEntityManager();
+            EntityTransaction transaction = entityManager.getTransaction();
+            transaction.begin();
+
+            Message message = entityManager.find(Message.class, 100L);
+            System.out.println(message.getText());
+            
+            transaction.commit();
+        }
+        finally {
+            if (entityManager != null) {
+                entityManager.close();
+            }
+            if (entityManagerFactory != null) {
+                entityManagerFactory.close();
+            }
+        }
+    }
 }

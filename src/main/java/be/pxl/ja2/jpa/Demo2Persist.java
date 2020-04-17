d6d395e2e58cff72b392c876c67a49e27dbe0c6c
Mariel <Mariel.LatellaBarros@student.pxl.be>
2020-04-17 09:33:38 +0200

    Use EntityManager to persist a new contact

--- src/main/java/be/pxl/ja2/jpa/Demo2Persist.java
+++ src/main/java/be/pxl/ja2/jpa/Demo2Persist.java
@@ -4,8 +4,12 @@ import be.pxl.ja2.jpa.model.Contact;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
-
+/** Demo2:
+ *  Use entityManager to create a contact and persist it in the database within a transaction
+ *  Close connections
+ */
 public class Demo2Persist {
 	public static void main(String[] args) {
 
@@ -14,8 +18,12 @@ public class Demo2Persist {
 		try {
 			entityManagerFactory = Persistence.createEntityManagerFactory("musicdb_pu");
 			entityManager = entityManagerFactory.createEntityManager();
+			EntityTransaction transaction = entityManager.getTransaction();
+
+			transaction.begin();
 			Contact sophie = new Contact("Sophie", 555666, "sophie@pxl.be");
 			entityManager.persist(sophie);
+			transaction.commit();
 		}
 		finally {
 			if (entityManager != null) {
